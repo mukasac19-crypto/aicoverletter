4db12d16caddacafa31f244e2a80ed3adfaffba0
Otim Isaac <isaacomega16@gmail.com>
2025-04-08 02:54:00 +0300

    setup redis connection

---
+++ lib/redis.ts
@@ -0,0 +1,10 @@
+// lib/redis.ts
+import { Redis } from 'ioredis'
+
+const connection = new Redis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: parseInt(process.env.REDIS_PORT || '6379', 10),
+  password: process.env.REDIS_PASSWORD || undefined,
+})
+
+export default connection
