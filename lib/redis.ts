b1a97bdb6a8aeb3e0b58e5bafddb91025a6aa12f
Otim Isaac <isaacomega16@gmail.com>
2025-04-08 04:14:26 +0300

    add maximum retries to redis setup

--- lib/redis.ts
+++ lib/redis.ts
@@ -5,6 +5,7 @@ const connection = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
   password: process.env.REDIS_PASSWORD || undefined,
+  maxRetriesPerRequest: null  // Add this line to resolve BullMQ error
 })
 
 export default connection
